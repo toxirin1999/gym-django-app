95230c03f46f9c8a6efdf04afee53ac021d0a749
David Millán Blanco <davidmillanblanco@Mac-mini-de-David.local>
2026-03-28 08:36:00 +0100

    Fix tipo_progresion + tipo de progresión por ejercicio completo

--- gymproject/settings.py
+++ gymproject/settings.py
@@ -102,8 +102,16 @@ WSGI_APPLICATION = 'gymproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'toxirin$gymdb',
+        'USER': 'toxirin',
+        'PASSWORD': 'ph2Xr4kd',
+        'HOST': 'toxirin.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
